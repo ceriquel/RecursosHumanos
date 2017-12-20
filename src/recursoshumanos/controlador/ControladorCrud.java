753d2f334760d17b6e13eebcf392c16153224730
Usuario <Usuario@LEARNINGCENTER>
2017-12-19 21:12:37 -0300

    agregar

--- src/recursoshumanos/controlador/ControladorCrud.java
+++ src/recursoshumanos/controlador/ControladorCrud.java
@@ -5,10 +5,20 @@
  */
 package recursoshumanos.controlador;
 
+import java.sql.SQLException;
+import java.util.Date;
+import recursoshumanos.modelo.ModeloCrud;
+
 /**
  *
  * @author Cèsar
  */
 public class ControladorCrud {
+    private ModeloCrud modeloCrud;
+    
+    public boolean agregaPersona(String rut, Date fechaIngreso, String direccion, Date fechaNacimiento, String nombre) throws SQLException{
+        modeloCrud=new ModeloCrud();
+        return modeloCrud.agregaPersona(rut, fechaIngreso, direccion, fechaNacimiento, nombre);
+    }
     
 }
--- src/recursoshumanos/modelo/ModeloCrud.java
+++ src/recursoshumanos/modelo/ModeloCrud.java
@@ -5,10 +5,26 @@
  */
 package recursoshumanos.modelo;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import recursoshumanos.conexion.Conexion;
+
 /**
  *
  * @author Cèsar
  */
 public class ModeloCrud {
-    
+
+    public boolean agregaPersona(String rut, Date fechaIngreso, String direccion, Date fechaNacimiento, String nombre) throws SQLException {
+        boolean flag = false;
+        System.out.println("Insertar registro...");
+        Statement dec;
+        dec = Conexion.conexion.createStatement();
+        String query1 = "insert into PERSONAL (RUT, FECHA_INGRESO, DIRECCION, FECHA_NACIMIENTO, NOMBRE) VALUES ('" + rut + "','" + fechaIngreso + "', '" + direccion + "', '" + fechaNacimiento + "','" + nombre + "')";
+        dec.executeUpdate(query1);
+        return flag = true;
+
+    }
+
 }
